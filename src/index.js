ed7651cceb2f94ae8ec577035e944cfe8cb13a48
ryan <rwong@timeplay.com>
2016-11-29 13:29:36 -0500

    new changes, expose tree

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import local from './decorator'
 import reducer from './reducer'
 import { stringifySafe } from './server'
 import Root from './root'
+import * as Tree from './tree'
 
 module.exports = {
   local,
   reducer,
   stringifySafe,
-  Root
+  Root,
+  Tree
 }
